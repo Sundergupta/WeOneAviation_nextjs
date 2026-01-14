53e5010e00644e3e39ddfc241afbf35cae653fb7
Sundergupta <sunder1234gupta@gmail.com>
2026-01-14 11:33:34 +0530

    upgrading meta title and description on syllabus  page

--- app/commercial-pilot-license/syllabus/page.jsx
+++ app/commercial-pilot-license/syllabus/page.jsx
@@ -1,7 +1,9 @@
 import SyllabusClient from "./SyllabusClient";
 
 export const metadata = {
-    title: "DGCA CPL Syllabus",
+    title: "Commercial Pilot License Syllabus",
+    description:
+        "Explore Commercial Pilot License syllabus at We One Aviation, covering DGCA ground subjects, flight training modules, and CPL exam preparation.",
 };
 
 export default function Page() {
